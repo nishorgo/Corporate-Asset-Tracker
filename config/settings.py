a8aff3a16fd01912b952b31e1e3307838f196b90
Abid Hasan <abidhasan.bh@outlook.com>
2024-03-07 00:24:39 +0600

    configured the automated api documentation

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,19 @@ from django.urls import path, include
 from rest_framework import routers
 from . import views 
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Asset Tracker API",
+        default_version='v1',
+    ),
+    public=True, 
+    permission_classes=[permissions.AllowAny], 
+)
+
 
 router = routers.DefaultRouter()
 router.register('companies', views.CompanyViewSet)
@@ -12,4 +25,6 @@ router.register('device-assignment', views.DeviceAssignment)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
